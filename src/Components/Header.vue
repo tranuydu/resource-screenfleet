b9a9b0ed84191ae158b900b86afb7be29e7f0174
Uy Du TRAN <tranuydu@gmail.com>
2018-05-19 18:06:49 +0200

    Recreate files

---
+++ src/Components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ul class="nav nav-pills">
+      <li role="presentation"><router-link to="/">Home</router-link></li>
+      <li role="presentation"><router-link to="/media">Media</router-link></li>
+    </ul>
+  </div>
+</template>
+
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import HomePage from './Components/Home.vue'
+import MediaPage from './Components/Media.vue'
+
+export const routes = [
+  { path: '/', component: HomePage},
+  { path: '/media', component: MediaPage}
+]
