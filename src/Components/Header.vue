5da5876cdd18847b267691aeb2f8ea73ad3c0d4c
Uy Du TRAN <tranuydu@gmail.com>
2018-05-19 18:01:24 +0200

    Delete all

--- src/Components/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <ul class="nav nav-pills">
-      <li role="presentation"><router-link to="/">Home</router-link></li>
-      <li role="presentation"><router-link to="/media">Media</router-link></li>
-    </ul>
-  </div>
-</template>
-
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import HomePage from './Components/Home.vue'
-import MediaPage from './Components/Media.vue'
-
-export const routes = [
-  { path: '/', component: HomePage},
-  { path: '/media', component: MediaPage}
-]
